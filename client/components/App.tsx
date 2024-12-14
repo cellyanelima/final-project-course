29a874fe8217ccab0c25b7e96790152823449958
K9Squad <kayne.menezes7@gmail.com>
2024-12-14 18:17:14 +1300

    "merged conflicts with Cellyane's income chart"

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,10 +4,10 @@ import FormField from './Form.tsx'
 
 function App() {
   return (
-    <div>
-      <IncomeExpenseChart />
-    </div>
     <>
+      <div>
+        <IncomeExpenseChart />
+      </div>
       <div className="app">
         <h1>My PiggyPal</h1>
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
