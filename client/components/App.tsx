71499ffa3190b3397492b56ea9ad376174ac0817
K9Squad <kayne.menezes7@gmail.com>
2024-12-16 08:27:30 +1300

    merging conflicts in App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import FormField from './Form'
 import Goals from './Goals'
+import IncomeExpenseChart from './IncomeExpenseChart'
 
 function App() {
   return (
@@ -9,14 +9,7 @@ function App() {
       <h1>My PiggyPal</h1>
       <FormField />
       <Goals />
-=======
-import IncomeExpenseChart from './IncomeExpenseChart'
-
-function App() {
-  return (
-    <div>
       <IncomeExpenseChart />
->>>>>>> b65c0ffbe8ca68f899f7daa920a2de4c42d4daa3
     </div>
   )
 }
