0a303e597338ee2f6bd1f67439acc28a86bda923
Marcos Ruiz <marcosruiz27@gmail.com>
2024-12-16 19:39:43 +1300

    Add User Overview files

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,16 +3,35 @@ import FormField from './Form'
 import BarChart from './BarChart.tsx'
 //import PieChart from './PieChart.tsx'
 //import { calculateAnnualExpenses } from '../hooks/useFinancialCalculations'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Overview from './Overview'
+//import IncomeExpenseChart from './IncomeExpenseChart'
 
 function App() {
   return (
-    <div className="app">
-      <h1>My PiggyPal</h1>
-      <FormField />
-      {/* <Goals /> */}
-      <BarChart />
-      {/* <PieChart /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/overview" element={<Overview />} />
+        <Route
+          path="/"
+          element={
+            <>
+              {/* <div>
+                <IncomeExpenseChart />
+              </div> */}
+              <div className="app">
+                <h1>My PiggyPal</h1>
+              </div>
+              <div>
+                <FormField />
+              </div>
+              <BarChart />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   )
 }
 
